807d5f9eed06661b8be7ae391ccb720afa43ffac
Jay Ireland <yousuc@users.noreply.github.com>
2023-08-02 15:16:50 -0500

    Update configuration.nix
    Added acpi and acpid

--- configuration.nix
+++ configuration.nix
@@ -143,6 +143,8 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    acpi
+    acpid
     alacritty
     autojump
     bash-completion
